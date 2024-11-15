8a4cd2680fd958fdf2ec7590cc6702dde32abc5c
RichyJames <D00254965@student.dkit.ie>
2024-11-15 12:57:34 +0000

    Define PlantType and Location models with name attribute

--- database/templates/app.py
+++ database/templates/app.py
@@ -8,8 +8,28 @@ app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 
 
-db = client.flask_database
-todos = db.todos
+class PlantType(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(100), nullable=False)
+
+class Location(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(100), nullable=False)   
+
+
+class Plant(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(100), nullable=False)
+    plant_type_id = db.Column(db.Integer, db.ForeignKey('plant_type.id'), nullable=False)
+    location_id = db.Column(db.Integer, db.ForeignKey('location.id'), nullable=False)
+    date_planted = db.Column(db.String(100), nullable=False)
+
+    plant_type = db.relationship('PlantType', backref='plants')
+    location = db.relationship('Location', backref='plants')
+    def __repr__(self):
+        return f'<Plant {self.name}>'
+
+
 
 @app.route('/')
 def home():
